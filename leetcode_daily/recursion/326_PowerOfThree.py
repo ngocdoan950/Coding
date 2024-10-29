d1c918a7ad9bbde152dba40d867dfd8bd4fd1307
ngocdoan950 <ngocdoannguyen950@gmail.com>
2024-10-29 17:28:56 +0700

    update

--- leetcode_daily/recursion/326_PowerOfThree.py
+++ leetcode_daily/recursion/326_PowerOfThree.py
@@ -7,14 +7,17 @@ def isPowerOfThree(n):
         return True
     if n % 3 != 0 or n == 0:
         return False
-    list_values = [n]
-    while (n % 3 == 0):
-        n = n /3
-        list_values.append(n)
-    if list_values[-1] == 1:
-        return True
-    else:
-        return False
+    #  solution 1
+    # list_values = [n]
+    # while (n % 3 == 0):
+    #     n = n /3
+    #     list_values.append(n)
+    # if list_values[-1] == 1:
+    #     return True
+    # else:
+    #     return False
+    #       solution 2: 
+    isPowerOfThree(n/3)
     
 if __name__ == "__main__":
     list_numb = [27, 0, -1, 1]
